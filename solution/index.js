c947fe557095bc41fe6a8798e11a8a3bb6265828
zivserphos <zivfromisrael@gmail.com>
2021-09-23 10:35:14 +0300

    move task when alt+1/2/3 pressed
    i create function that occurs with the help of event listeners that allow the user to move tasks from a section to section using alt+/1/2/3

--- solution/index.js
+++ solution/index.js
@@ -1,8 +1,9 @@
 let tasks = window.localStorage.getItem("tasks")
+let altpressed = false;
+let currentEl;
 let localSave;
 if (!tasks)
 {
-    alert("s")
     tasks = {
         "todo": [],
         "in-progress": [],
@@ -50,6 +51,7 @@ function editTask (e)
 {
     if (e.target.tag = "li")
     {
+       
        const changeTask = document.createElement("input")
        changeTask.innerHTML = e.target.innerHTML
        e.parentNode.replaceChild(changeTask, e.target);
@@ -63,7 +65,6 @@ function enterButton(e)
        const inputTag = document.getElementById(e.target.id.split("submit-")[1]+ "-task")
        const taskInput = inputTag.value
        const ul = document.querySelector("." + inputTag.id.split("add-")[1] + "s")
-       console.log(ul)
        const li = createElement("li", [] , [] , {draggable: "true"})
        li.addEventListener("dblclick" , function(e) {editTask(e)})
        li.classList.add("task")
@@ -83,6 +84,69 @@ function enterButton(e)
     }
 }
 
+function moveTaskToSection(id)
+{
+    if(currentEl && currentEl.closest("ul").id !== id)
+    {
+        localSave[id].unshift(currentEl.textContent)
+        const indexOfTask = localSave[currentEl.closest("ul").id].indexOf(currentEl.textContent)
+        localSave[currentEl.closest("ul").id].splice(localSave[currentEl.closest("ul").id].indexOf(currentEl.textContent))
+        localStorage.setItem("tasks", JSON.stringify(localSave))
+        document.getElementById(id).append(currentEl)
+    }
+}
+
+function altPressed(event)
+{
+    if (event.key === "Alt")
+    {
+          altpressed = true;  
+    }
+}
+
+function altGone(event)
+{
+    altpressed = false
+}
+
+function changeTaskSection(event) // check if the user type a nubmer from 1 to 3 while the alt key was pressed and thats mean should move the task a section
+{
+    if(altpressed && event.key === "1")
+    {
+        moveTaskToSection("todo")
+    }
+    else if (altpressed && event.key === "2")
+    {
+        moveTaskToSection("in-progress")
+    }
+    else if (altpressed && event.key === "3")
+    {
+        moveTaskToSection("done")
+    }
+    
+}
+
+function hoverElement(event)
+{
+    if(event.target.tagName === "LI")
+    {
+        currentEl = event.target;
+    }
+}
+
+function outOfElement(event)
+{
+    if(event.target.tagName === "LI")
+    {
+        currentEl = null
+    }
+}
 
 document.addEventListener("click" ,  function(e){enterButton(e)})
+document.addEventListener("keydown" , function(event){altPressed(event)})
+document.addEventListener("keyup" , function(event){altGone(event)})
+document.addEventListener("keydown", function(event){changeTaskSection(event)})
+document.addEventListener("mouseover" , function(event){hoverElement(event)})
+document.addEventListener("mouseout" ,  function(event){outOfElement(event)})
+
 //window.localStorage.setItem("tasks", {todo: [] , "in-progress": [] , done: []} )
