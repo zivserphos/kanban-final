beea002857c0a386dc26b6f8cee5e155a80c4e7b
zivserphos <zivfromisrael@gmail.com>
2021-10-11 21:59:27 +0300

    change according to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,19 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es6: true
     },
-    "extends": [
-        "airbnb-base"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 13,
+    extends: ["airbnb-base" , "prettier"],
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+    } ,
+    parserOptions: {
+        "ecmaVersion": 2018,
         "sourceType": "module"
     },
-    "rules": {
+    plugins: ['prettier'],
+    rules: {
+        "prettier/prettier": "error"
     }
 };
